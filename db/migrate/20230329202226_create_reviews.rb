17876ed391162427626cdced6e231c73e6880b49
Blake Creasser <blakecreasser@aggienetwork.com>
2023-03-29 15:24:39 -0500

    finish reviews table migration table

--- db/migrate/20230329202226_create_reviews.rb
+++ db/migrate/20230329202226_create_reviews.rb
@@ -1,5 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
-    
+    create_table :reviews do |t|
+      t.integer :snowboard_id
+      t.integer :rating
+      t.string :comment
+    end
   end
 end
